7916806424530b7ec860aa0ba132ba193cc9876d
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-11 18:38:56 +0100

    pagination response class

--- src/main/java/com/github/manimovassagh/blog/payload/PostResponse.java
+++ src/main/java/com/github/manimovassagh/blog/payload/PostResponse.java
@@ -1,11 +1,14 @@
 package com.github.manimovassagh.blog.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 
 @Data
+@AllArgsConstructor
 public class PostResponse {
     private List<PostDTO> content;
     private int pageNo;
