a5d8a99fe61ae16d8bc73ec43f23199910fad26b
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-12 11:30:05 +0100

    asc sorting

--- src/main/java/com/github/manimovassagh/blog/utils/AppConstants.java
+++ src/main/java/com/github/manimovassagh/blog/utils/AppConstants.java
@@ -1,10 +1,15 @@
 package com.github.manimovassagh.blog.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
 public class AppConstants {
-    public static final String DEFAULT_PAGE_NUMBER="0";
-    public static final String DEFAULT_PAGE_SIZE="10";
-    public static  final String DEFAULT_SORT_BY="id";
-    public static  final  String   DEFAULT_SORT_DIRECTION="asc";
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String DEFAULT_SORT_BY = "id";
+    public static final String DEFAULT_SORT_DIRECTION = "asc";
 
 
 }
