cf92bc1e81b5b1a72578d7d3d28d43816d5f48ba
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-12 22:03:12 +0100

    add repo name

--- src/main/java/com/github/manimovassagh/blog/repository/RoleRepository.java
+++ src/main/java/com/github/manimovassagh/blog/repository/RoleRepository.java
@@ -3,5 +3,9 @@ package com.github.manimovassagh.blog.repository;
 import com.github.manimovassagh.blog.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role,Long> {
+
+    Optional<Role> findByName(String name);
 }
