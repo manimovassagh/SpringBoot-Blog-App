c55fa405b8d39c20a6ef2103e8eacb097f37602a
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-12 11:32:02 +0100

    make constants

--- src/main/java/com/github/manimovassagh/blog/controller/PostController.java
+++ src/main/java/com/github/manimovassagh/blog/controller/PostController.java
@@ -4,6 +4,7 @@ package com.github.manimovassagh.blog.controller;
 import com.github.manimovassagh.blog.payload.PostDTO;
 import com.github.manimovassagh.blog.payload.PostResponse;
 import com.github.manimovassagh.blog.service.PostService;
+import com.github.manimovassagh.blog.utils.AppConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,10 +32,10 @@ public class PostController {
      */
     @GetMapping
     public PostResponse getAllPosts
-    (@RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-     @RequestParam(value = "pageSize", defaultValue = "5", required = false) int pageSize,
-     @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-     @RequestParam(value = "sortDir",defaultValue = "asc",required = false) String sortDir) {
+    (@RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+     @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+     @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+     @RequestParam(value = "sortDir",defaultValue = AppConstants.DEFAULT_SORT_DIRECTION,required = false) String sortDir) {
         return postService.getAllPosts(pageNo, pageSize, sortBy,sortDir);
     }
 
--- src/main/java/com/github/manimovassagh/blog/utils/AppConstants.java
+++ src/main/java/com/github/manimovassagh/blog/utils/AppConstants.java
@@ -1,8 +1,6 @@
 package com.github.manimovassagh.blog.utils;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 
 public class AppConstants {
