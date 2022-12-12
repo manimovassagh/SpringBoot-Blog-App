feeb2ea9ce543d496ff2def3c34ad036ba8272b3
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-12 17:14:04 +0100

    add validation

--- src/main/java/com/github/manimovassagh/blog/payload/PostDTO.java
+++ src/main/java/com/github/manimovassagh/blog/payload/PostDTO.java
@@ -1,7 +1,10 @@
 package com.github.manimovassagh.blog.payload;
 
 import com.github.manimovassagh.blog.entity.Comment;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.util.Set;
 
@@ -10,8 +13,14 @@ import java.util.Set;
 public class PostDTO {
 
     private Long id;
+    @NotEmpty
+    @Size(min = 2,message = "Post title should have at least 2 characters")
     private  String title;
+    @NotEmpty
+    @Size(min = 10,message = "Post description should have at least 10 characters")
+
     private String description;
+    @NotEmpty
     private  String content;
 
     private Set<CommentDto> comments;
