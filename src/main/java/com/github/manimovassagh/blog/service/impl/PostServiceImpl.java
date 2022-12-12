3960dbd5d0934a484d8adc94ecb4518cb4334217
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-12 11:08:35 +0100

    asc sorting

--- src/main/java/com/github/manimovassagh/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/github/manimovassagh/blog/service/impl/PostServiceImpl.java
@@ -11,11 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 
