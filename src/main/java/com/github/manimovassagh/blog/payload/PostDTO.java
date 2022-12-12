57f09f63b5d23535d8db3bef4a100e6036080bf9
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-12 18:53:24 +0100

    extra validation

--- src/main/java/com/github/manimovassagh/blog/payload/PostDTO.java
+++ src/main/java/com/github/manimovassagh/blog/payload/PostDTO.java
@@ -14,16 +14,16 @@ public class PostDTO {
 
     private Long id;
     @NotEmpty
-    @Size(min = 2,message = "Post title should have at least 2 characters")
-    private  String title;
+    @Size(min = 2, message = "Post title should have at least 2 characters")
+    private String title;
     @NotEmpty
-    @Size(min = 10,message = "Post description should have at least 10 characters")
+    @Size(min = 10, message = "Post description should have at least 10 characters")
 
     private String description;
     @NotEmpty
-    @Size(min = 2,message = "Post content should have at least 2 characters")
-
-    private  String content;
+    @Size(min = 2, message = "Post content should have at least 2 characters")
+    @Size(min = 1, message = "Post content should not be Empty! ")
+    private String content;
 
     private Set<CommentDto> comments;
 }
