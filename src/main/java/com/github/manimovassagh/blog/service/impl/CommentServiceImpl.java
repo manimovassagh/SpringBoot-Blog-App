3e7932427760d842d4e88cca44d1fcf9cf29cf80
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-12 12:10:29 +0100

    lambda

--- src/main/java/com/github/manimovassagh/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/github/manimovassagh/blog/service/impl/CommentServiceImpl.java
@@ -1,14 +1,51 @@
 package com.github.manimovassagh.blog.service.impl;
 
+import com.github.manimovassagh.blog.entity.Comment;
+import com.github.manimovassagh.blog.entity.Post;
+import com.github.manimovassagh.blog.exception.ResourceNotFoundException;
 import com.github.manimovassagh.blog.payload.CommentDto;
+import com.github.manimovassagh.blog.repository.CommentRepository;
+import com.github.manimovassagh.blog.repository.PostRepository;
 import com.github.manimovassagh.blog.service.CommentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+
     @Override
     public CommentDto createComment(long postId, CommentDto commentDto) {
+        Comment comment = mapToEntity(commentDto);
+        //retrieve Post Entity base on ID
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
         return null;
     }
+
+
+    private CommentDto mapToDto(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setName(comment.getName());
+        commentDto.setBody(comment.getBody());
+        commentDto.setEmail(comment.getEmail());
+        return commentDto;
+    }
+
+
+    private Comment mapToEntity(CommentDto commentDto) {
+        Comment comment = new Comment();
+        comment.setId(commentDto.getId());
+        comment.setName(commentDto.getName());
+        comment.setBody(commentDto.getBody());
+        comment.setEmail(commentDto.getEmail());
+
+        return comment;
+    }
+
+
 }
