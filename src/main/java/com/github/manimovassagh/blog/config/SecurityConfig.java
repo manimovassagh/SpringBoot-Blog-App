5aadfc5af4ed4b8f2ee92e8970db9a37229a5eaf
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-13 01:31:56 +0100

    change auth to keyclocak

--- src/main/java/com/github/manimovassagh/blog/config/SecurityConfig.java
+++ src/main/java/com/github/manimovassagh/blog/config/SecurityConfig.java
@@ -1,17 +1,10 @@
 package com.github.manimovassagh.blog.config;
 
-import com.github.manimovassagh.blog.security.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final CustomUserDetailService customUserDetailService;
-
-
-
-
 
 }
--- src/main/java/com/github/manimovassagh/blog/security/CustomUserDetailService.java
+++ src/main/java/com/github/manimovassagh/blog/security/CustomUserDetailService.java
@@ -5,36 +5,3 @@ import com.github.manimovassagh.blog.entity.User;
 import com.github.manimovassagh.blog.exception.ResourceNotFoundException;
 import com.github.manimovassagh.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-
-@Service
-@RequiredArgsConstructor
-public class CustomUserDetailService implements UserDetailsService {
-
-    private final UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
-        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with user name or email" + usernameOrEmail));
-        return new org.springframework.security.core.userdetails.User(user.getEmail()
-                , user.getPassword(),mapRolesToAuthorities(user.getRoles()
-        ));
-
-    }
-
-private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Set<Role> roles){
-   return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
-}
-}
