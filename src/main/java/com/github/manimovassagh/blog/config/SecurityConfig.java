4eddfe66353a3b4167681822cdd8a8430e33899d
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-25 18:58:22 +0100

    config SecurityConfig

--- src/main/java/com/github/manimovassagh/blog/config/SecurityConfig.java
+++ src/main/java/com/github/manimovassagh/blog/config/SecurityConfig.java
@@ -1,11 +1,22 @@
 package com.github.manimovassagh.blog.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
 
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
+
+}
