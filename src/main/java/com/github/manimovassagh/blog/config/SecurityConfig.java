ab1ef3b999fdc952929380fe4ffbe9b7c172d056
Mani Movassagh <mani.mgh@gmail.com>
2022-12-25 19:48:22 +0100

    encode password

--- src/main/java/com/github/manimovassagh/blog/config/SecurityConfig.java
+++ src/main/java/com/github/manimovassagh/blog/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,6 +17,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Bean
+    static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,16 +29,16 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean   
+    @Bean
     UserDetailsService userDetailsService() {
         UserDetails mani = User.builder()
                 .username("mani")
-                .password("mmmmmm")
+                .password(passwordEncoder().encode("mmmmmm"))
                 .roles("User").build();
 
         UserDetails admin = User.builder()
                 .username("admin")
-                .password("admin")
+                .password(passwordEncoder().encode("admin"))
                 .roles("ADMIN").build();
         return new InMemoryUserDetailsManager(mani, admin);
     }
