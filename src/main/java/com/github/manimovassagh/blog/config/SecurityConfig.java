533575dd8200963db9df4a485b7b0bda3290b6cd
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-25 19:36:23 +0100

    in memory

--- src/main/java/com/github/manimovassagh/blog/config/SecurityConfig.java
+++ src/main/java/com/github/manimovassagh/blog/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +20,15 @@ public class SecurityConfig {
         http.csrf().disable().authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+
+    UserDetailsService userDetailsService(){
+        UserDetails mani= User.builder()
+                .username("mani")
+                .password("mmmmmm")
+                .roles("ADMIN").build();
+        return null;
     }
 
 }
