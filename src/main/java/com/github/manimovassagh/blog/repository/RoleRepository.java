76bf2072133745a4ab0d582a0b96f2060d68b857
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-28 22:43:33 +0100

    grant autority

--- src/main/java/com/github/manimovassagh/blog/repository/RoleRepository.java
+++ src/main/java/com/github/manimovassagh/blog/repository/RoleRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role,Long> {
-
+//name
     Optional<Role> findByName(String name);
 }
