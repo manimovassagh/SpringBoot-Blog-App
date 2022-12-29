c53785373473d239856a897ec1b54495d55dae28
Mani Movassagh <mani.mgh@gmail.com>
2022-12-29 13:22:11 +0100

    define auth

--- src/main/java/com/github/manimovassagh/blog/service/impl/AuthServiceImpl.java
+++ src/main/java/com/github/manimovassagh/blog/service/impl/AuthServiceImpl.java
@@ -2,10 +2,27 @@ package com.github.manimovassagh.blog.service.impl;
 
 import com.github.manimovassagh.blog.payload.LoginDto;
 import com.github.manimovassagh.blog.service.serviceInterface.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+
+    private final AuthenticationManager authenticationManager;
+
     @Override
     public String login(LoginDto loginDto) {
-        return null;
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail()
+                        , loginDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "User Logged in Successfully !!!";
     }
-}
+}   
