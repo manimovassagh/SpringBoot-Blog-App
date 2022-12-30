41d4ac8ae6e8801ad139af65ef44d647effbf483
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-31 00:50:02 +0100

    define search query

--- src/main/java/com/github/manimovassagh/blog/repository/PostRepository.java
+++ src/main/java/com/github/manimovassagh/blog/repository/PostRepository.java
@@ -6,10 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface PostRepository  extends JpaRepository<Post,Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("SELECT p FROM Post p WHERE " +
-            "p.title LIKE CONCAT ('%',:query,'%')"+
-    "OR p.description LIKE CONCAT ('%',:query,'%')")
+            "p.title LIKE CONCAT ('%',:query,'%')" +
+            "OR p.description LIKE CONCAT ('%',:query,'%')")
     List<Post> searchPost(String query);
+
+    //native query sample
+    @Query(value = "SELECT * FROM Post p WHERE " +
+            "p.title LIKE CONCAT ('%',:query,'%')" +
+            "OR p.description LIKE CONCAT ('%',:query,'%')", nativeQuery = true)
+    List<Post> searchPostNative(String query);
 }
